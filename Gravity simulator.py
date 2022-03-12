4a490cc440fd5d1f6e68b5a48bbb7d80166d597b
BenTurpin94 <101436616+BenTurpin94@users.noreply.github.com>
2022-03-12 18:51:34 +0000

    Update Gravity simulator.py

--- Gravity simulator.py
+++ Gravity simulator.py
@@ -5,37 +5,46 @@ from operator import mod
 
 
 
-angle = int(input("Enter angle of ball"))
-force  = int(input("Enter force of ball"))
-mass= int(input("mass of ball"))
-time_interval=int(input("select time intervals"))
-total_time=int(input("select total time"))
+angle = float(input("Enter angle of ball"))
+velocity  = float(input("Enter velocity  of ball"))
+mass= float(input("mass of ball"))
+time_interval=float(input("select time intervals"))
+total_time=float(input("select total time"))
 
-def accelerationx(force,angle,mass):
-    return int(force*math.cos(angle)/mass)
+def intvelx(velocity,angle):
+    return float(velocity*math.cos(angle))
+
+def intvely(velocity,angle):
+    return float(velocity*math.sin(angle))    
+
+
+#def accelerationx(force,angle,mass):
+ #   return float(force*math.cos(angle)/mass)
 
 accelerationy=-9.81
 
-def velocityx(accelerationx,time):
-    return int(accelerationx(force, mass, angle)*time)
+def velocityx(intvelx):
+    return float(intvelx)
 
-def velocityy(accelerationy,time):
-    return int(accelerationy*time)
+def velocityy(intvely,accelerationy,time):
+    return float(intvely+accelerationy*time)
 
-def distancex(accelerationx,time):
-    return int(0.5*accelerationx(force, mass, angle)*time**2)
+def distancex(intvelx,time):
+    return float(intvelx*time)
 
-def distancey(accelerationy, time):
-    return int(0.5*accelerationy*time**2)
+def distancey(accelerationy,time,velocityy,intvely):
+    return float(velocityy(intvely,accelerationy,time)*time + 0.5*accelerationy*time**2)
 
 time=0
 while time < total_time:
-    print("Acceleration at time t=",time, "(",accelerationx(force,angle,mass),accelerationy,")")
-    print("Velocity at time t=",time,"(",velocityx(accelerationx,time),velocityy(accelerationy,time),")")
-    print("Posesion at time t=",time,"(",distancex(accelerationx,time),distancey(accelerationy,time),")")
+    print("Acceleration at time t=",time, "(",0,accelerationy,")")
+    print("Velocity at time t=",time,"(",velocityx(intvelx(velocity,angle)),velocityy(intvely(velocity,angle),accelerationy,time),")")
+    print("Posesion at time t=",time,"(",distancex(intvelx(velocity,angle),time),distancey(accelerationy,time,velocityy(intvely(velocity,angle),accelerationy,time),intvely(velocity,angle)),")")
     time += time_interval
-    plt.plot(distancex,distancey)
-plt.show()
+   # plt.plot(float(distancex(intvelx,time)),float(distancey(accelerationy,time,velocityy)),'o')
+    #if float(distancey(accelerationy,time,velocityy)) < 0:
+    #    break
+#plt.show()
 
 
 
