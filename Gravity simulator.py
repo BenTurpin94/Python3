7f14f91c51e8ecd7e4d5d161bcc79f32863fcd94
BenTurpin94 <101436616+BenTurpin94@users.noreply.github.com>
2022-03-12 02:28:22 +0000

    Update Gravity simulator.py

--- Gravity simulator.py
+++ Gravity simulator.py
@@ -1,27 +1,19 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 from operator import mod
 
 
-def main()
-    angle = int(input("Enter angle of ball"))
-    force  = int(input("Enter force of ball"))
-    mass= int(input("mass of ball"))
-    time_interval=int(input("select time intervals"))
-    total_time=int(input("select total time"))
-    
-    time=0
-    while time < total_time:
-        print("Acceleration at time t=",time, "(",accelerationx(force,angle,mass),accelerationy,")")
-        print("Velocity at time t=",time,"(",velocityx(accelerationx,time),velocityy(accelerationy,time),")")
-        print("Posesion at time t=",time,"(",distancex(accelerationx,time),distancey(accelerationy,time),")")
-        time += time_interval
 
+angle = int(input("Enter angle of ball"))
+force  = int(input("Enter force of ball"))
+mass= int(input("mass of ball"))
+time_interval=int(input("select time intervals"))
+total_time=int(input("select total time"))
 
-# this is a funtion so needs to be called like accelerationx(force, mass, angle) or wont work
 def accelerationx(force,angle,mass):
     return int(force*math.cos(angle)/mass)
 
-# this is a value so can be called without the brackets
 accelerationy=-9.81
 
 def velocityx(accelerationx,time):
@@ -36,9 +28,18 @@ def distancex(accelerationx,time):
 def distancey(accelerationy, time):
     return int(0.5*accelerationy*time**2)
 
+time=0
+while time < total_time:
+    print("Acceleration at time t=",time, "(",accelerationx(force,angle,mass),accelerationy,")")
+    print("Velocity at time t=",time,"(",velocityx(accelerationx,time),velocityy(accelerationy,time),")")
+    print("Posesion at time t=",time,"(",distancex(accelerationx,time),distancey(accelerationy,time),")")
+    time += time_interval
+    plt.plot(distancex,distancey)
+plt.show()
 
-if __name__ == "__main__":
-    main()
+
+
+
 
 
 
